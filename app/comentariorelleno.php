377097e85af5b5fc3b930bc95192f366599eeeea
gonzaloweb <72246884+gonzaloweb@users.noreply.github.com>
2020-11-07 18:07:29 +0100

    Add files via upload

--- app/comentariorelleno.php
+++ app/comentariorelleno.php
@@ -3,7 +3,7 @@ Tema<br>
  <input type="text" name="tema" size=30 
    value="<?=(isset($_REQUEST['tema']))? evitaInyeccion($_REQUEST['tema']):''?>" ><br>
 Comentario: <br>
-<textarea name="comentario" rows="4" cols="50"><?=(isset($_REQUEST['comentario']))?$_REQUEST['comentario']:''?></textarea>
+<textarea name="comentario" rows="4" cols="50"><?=(isset($_REQUEST['comentario']))? evitaInyeccion($_REQUEST['comentario']):''?></textarea>
 <br><br>
 <input type="submit" name="orden" value="Detalles">
 <input type="submit" name="orden" value="Nueva opinión">
